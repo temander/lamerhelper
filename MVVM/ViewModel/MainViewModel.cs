f7bf78416759d028cbbe26a2fc2727eb24cd8b0f
darkfated <darkfated@icloud.com>
2025-02-26 05:42:21 +0300

    Система переключения вкладок

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using LamerHelper.Core;
+
+namespace LamerHelper.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand OptimizationViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public OptimizationViewModel OptVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            OptVM = new OptimizationViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            OptimizationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = OptVM;
+            });
+        }
+    }
+}
---
+++ MVVM/ViewModel/OptimizationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamerHelper.MVVM.ViewModel
+{
+    class OptimizationViewModel
+    {
+    }
+}
