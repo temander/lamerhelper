65c083f0eae6720c54bc73450154cede716b173d
darkfated <darkfated@icloud.com>
2025-03-05 06:12:09 +0300

    Завершение процесса после ошибки

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,7 +13,7 @@ namespace LamerHelper
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             MessageBox.Show($"Ошибка: {e.Exception.Message}", "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
-            e.Handled = true;
+            //e.Handled = true; 
         }
     }
 }
