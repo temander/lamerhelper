1ad8cbe8a3d72503c232c1a4c0d3c445b997268d
darkfated <darkfated@icloud.com>
2025-03-05 03:42:22 +0300

    Добавлено уведомление при ошибке программы

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace LamerHelper
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Ошибка: {e.Exception.Message}", "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
